5e61a218351d3203f689adb157fe6e9314105e81
MrELT <99138595+MrELT@users.noreply.github.com>
2025-01-05 21:44:07 +0300

    Add files via upload

---
+++ Blogs/Blogs.js
@@ -0,0 +1,32 @@
+document.getElementById('info_page').addEventListener('click', function () {
+    window.location.href = '../Information/Information.html';
+});
+document.getElementById('name_link').addEventListener('click', function () {
+    window.location.href = '../Information/Information.html';
+});
+document.getElementById('portfolio_page').addEventListener('click', function () {
+    window.location.href = '../Portfolio/Portfolio.html';
+});
+document.getElementById('contact_page').addEventListener('click', function () {
+    window.location.href = '../Contact/Contact.html';
+});
+document.getElementById('home_button').addEventListener('click', function () {
+    window.location.href = '../Home/home_page.html';
+});
+document.getElementById('blogs_page').addEventListener('click', function () {
+    window.location.href = 'Blogs.html';
+});
+document.getElementById('blog-1').addEventListener('click', function () {
+    window.location.href = 'Blog-1/blog-1.html';
+});
+function executeSafeCode() {
+    console.log("This is safe code!");
+}
+
+// Call the function directly
+setTimeout(executeSafeCode, 1000);
+
+// Or use anonymous functions
+setTimeout(() => {
+    console.log("Safe code without eval!");
+}, 1000);
