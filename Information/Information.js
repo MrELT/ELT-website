d2e9a83165ea05f6d7c0d7582cbd2e0b1b9da2d5
MrELT <99138595+MrELT@users.noreply.github.com>
2025-01-12 00:23:36 +0300

    Add files via upload

---
+++ Information/Information.js
@@ -0,0 +1,22 @@
+document.getElementById('info_page').addEventListener('click', function () {
+    window.location.href = 'Information.html';
+});
+document.getElementById('name_link').addEventListener('click', function () {
+    window.location.href = 'Information.html';
+});
+document.getElementById('blogs_page').addEventListener('click', function () {
+    window.location.href = '/Blogs/Blogs.html';
+});
+document.getElementById('portfolio_page').addEventListener('click', function () {
+    window.location.href = '/Portfolio/Portfolio.html';
+});
+document.getElementById('contact_page').addEventListener('click', function () {
+    window.location.href = '/Contact/Contact.html';
+});
+document.getElementById('home_button').addEventListener('click', function () {
+    window.location.href = '/Home/home_page.html';
+});
+
+function executeSafeCode() {
+    console.log("This is safe code!");
+}
